f9de4edfb23cbc2a218a875059e64d7177b7d05a
Sai Manoj Yarlagadda <ys9483@srmist.edu.in>
2023-10-05 18:40:13 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ st.image(image_path, use_column_width=True)
 
 st.title("Boston House Price Prediction App")
 
-# Collect input values for all 8 features
 crim = st.number_input("Crime Rate (CRIM):")
 zn = st.number_input("Proportion of Residential Land Zoned (ZN):")
 indus = st.number_input("Proportion of Non-Retail Business Acres (INDUS):")
@@ -21,13 +20,9 @@ rm = st.number_input("Average Number of Rooms (RM):")
 age = st.number_input("Proportion of Owner-Occupied Units Built Before 1940 (AGE):")
 dis = st.number_input("Weighted Distances to Employment Centers (DIS):")
 
-# Add a button to trigger prediction
 if st.button("Predict"):
-    # Create input data array
     input_data = np.array([[crim, zn, indus, chas, nox, rm, age, dis]])
-
-    # Make predictions
     predicted_value = svr_model.predict(input_data)
-
     st.write(f'Predicted House Price: ${predicted_value[0] * 1000:.2f}')
+
 st.markdown("Made with ❤️ by [Sai Manoj Yarlagadda](https://www.linkedin.com/in/sai-manoj-yarlagadda-a6159b225/)")
